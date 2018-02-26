4c58717ecb49036813f2ead1b63b4c683b77a89e
Alvaro C <toporniz@gmail.com>
2018-02-26 10:41:00 +0100

    Añadida la index y los controladores y daos para la inserción de usuario en bd mediante JSON (#6)

---
+++ EmergyaFilms/src/main/java/com/appfilm/film/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.appfilm.film.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+/**
+ * 
+ * @author fjmartinez
+ *	Clase WebConfig para añadir manejadores de la web y rutas de las jsp
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+	}
+	
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+		
+	}
+	
+
+}
---
+++ EmergyaFilms/src/main/java/com/appfilm/film/dao/UsuarioDao.java
@@ -0,0 +1,31 @@
+package com.appfilm.film.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.appfilm.film.model.Usuario;
+
+
+@Repository
+@Transactional
+public class UsuarioDao {
+	
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+    
+	
+	/*
+     * Almacena el usuario en la base de datos
+     */
+    public void create(Usuario usuario) {
+    	entityManager.persist(usuario);
+    	return;
+    }
+    
+	
+
+}
