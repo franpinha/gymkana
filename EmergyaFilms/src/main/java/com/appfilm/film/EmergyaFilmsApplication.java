18526a535402e7b880fde1c44a5a6185153b99bb
franpinha <36632575+franpinha@users.noreply.github.com>
2018-03-05 11:14:09 +0100

    @franpinha -#07- Cambios en pom.xml para despliegue en tomcat,optimizacion de controladores y validadores (#15)

--- EmergyaFilms/src/main/java/com/appfilm/film/EmergyaFilmsApplication.java
+++ EmergyaFilms/src/main/java/com/appfilm/film/EmergyaFilmsApplication.java
@@ -2,11 +2,19 @@ package com.appfilm.film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmergyaFilmsApplication {
+public class EmergyaFilmsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmergyaFilmsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		
+		return application.sources(EmergyaFilmsApplication.class);
 	}
 }
